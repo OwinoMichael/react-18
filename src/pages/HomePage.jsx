ec8d1e1bee473e218f4fe8a658bca2b497632bd5
OwinoMichael <owinomichael1400@gmail.com>
2025-01-03 20:13:18 -0500

    Links from react router dom

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import HomeCards from '../components/HomeCards';
+import JobListings from '../components/JobListings.jsx'
+import ViewAllJobs from '../components/ViewAllJobs.jsx';
 
 const HomePage = () => {
   return (
     <>
     <Hero/>
     <HomeCards />
+    <JobListings/>
+    <ViewAllJobs/>
     </>
   )
 }
